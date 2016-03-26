4b8ad0d25c05288e59689a2af41c0f506682ff32
andilazvegas <andi.lazvegas@gmail.com>
2016-03-26 13:59:48 +0700

    Change About Layout

--- PALS/src/com/gopals/pals/About.java
+++ PALS/src/com/gopals/pals/About.java
@@ -1,7 +1,12 @@
 package com.gopals.pals;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -10,15 +15,31 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class About extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
-
+		
+		ActionBar bar = getActionBar();
+		bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#1a1a1a")));
+		
 		String versionName = getResources().getString(R.string.versionName);
+		Typeface bariol = Typeface.createFromAsset(getAssets(), "fonts/bariol.ttf");
+		TextView aboutLbl = (TextView) findViewById(R.id.aboutLbl);
+		TextView devLbl = (TextView) findViewById(R.id.developersLbl);
+		TextView copyrightLbl = (TextView) findViewById(R.id.copyrightLbl);
+		TextView versionLbl = (TextView) findViewById(R.id.versionLbl);
 		TextView version = (TextView) findViewById(R.id.versionName);
+		TextView rateLbl = (TextView) findViewById(R.id.rateLbl);
 		version.setText(versionName);
+		
+		aboutLbl.setTypeface(bariol, Typeface.BOLD);
+		devLbl.setTypeface(bariol);
+		copyrightLbl.setTypeface(bariol);
+		versionLbl.setTypeface(bariol);
+		version.setTypeface(bariol);
+		rateLbl.setTypeface(bariol);
 
 		LinearLayout rate = (LinearLayout) findViewById(R.id.rateApps);
 		rate.setOnClickListener(new OnClickListener() {
@@ -34,5 +55,4 @@ public class About extends Activity {
 		});
 
 	}
-
 }
